40b44792061c89cd59ab53824024d1da0192cd27
LightIce10 <145585156+LightIce10@users.noreply.github.com>
2023-11-24 12:27:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// INPUT
+document.getElementById("size").addEventListener("click", newOrder);
+document.getElementById("first").addEventListener("click", firstOne);
+document.getElementById("second").addEventListener("click", secondOne);
+
+// function
+function newOrder() {}
+
+// OUTPUT
