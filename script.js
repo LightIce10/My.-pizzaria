c5b15fcf9b9b65607f984348951da92b97907ccb
LightIce10 <145585156+LightIce10@users.noreply.github.com>
2023-11-30 12:17:24 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,22 +8,20 @@ document.getElementById("btn").addEventListener("click", doneOrder);
 
 // function
 function doneOrder() {
-  let a = document.getElementById("btn").value;
-  let b = document.getElementById("size").value;
-  let c = document.getElementById("first").value;
-  let d = document.getElementById("second").value;
+  let sizes = document.getElementById("size").value;
+  let fT = document.getElementById("first").value;
+  let sT = document.getElementById("second").value;
 
   // testing
-  console.log(a);
-  console.log(b);
-  console.log(c);
-  console.log(d);
+  console.log(sizes);
+  console.log(fT);
+  console.log(sT);
 
   // process
-  let changetext = `You have ordered a ${b} pizza with:
+  let changetext = `You have ordered a ${sizes} pizza with:
   <ul>
-   <li>${c}</li> 
-   <li>${d} </li>
+   <li>${fT}</li> 
+   <li>${sT} </li>
     </ul>
    Thank you for ordering from <a href="https://www.pizza73.com/store/1/delivery" target="_blank">Pizza 73</a>! Your pizza will be ready in 20 minutes!`;
 
